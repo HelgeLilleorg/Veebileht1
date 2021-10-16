b8c15e770dcf5dd8f4537dfbded3fb01459835d3
HelgeLilleorg <helge.lilleorg@gmail.com>
2021-10-16 14:06:59 +0300

    JSON getting pics works

--- script.js
+++ script.js
@@ -4,18 +4,18 @@
 
     for (post of posts) {
         let div = $('<article class= "unit">');
-        let user = $('<div class="header-section-1">').text(post.user);
+        let user = $('<div class="site-logo">').text(post.user);
         let datetime = $('<div class="header-section-2">').text(post.datetime);
        // let content = $('<div class="middlepart">').text(post.content);
         
         
-       let body = $('<h4>').text(post.body);
+       let content = $('<h4>').text(post.content);
        let image = $('<img>').text(post.image);
        let bottom = $('<div class="bottom-part"></div>').text(post.bottom);
 
         div.append(user);
         div.append(datetime);
-        div.append(body);
+        div.append(content);
         div.append(image);
         div.append(bottom);
 
